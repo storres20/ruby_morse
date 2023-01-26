0d6f1dfecc1281a8805072af1e8f7e029b196a70
Italo Lon kan <sanchotorres777@gmail.com>
2023-01-26 15:26:07 -0500

    Fix linter's error message

--- morsecode.rb
+++ morsecode.rb
@@ -1,7 +1,3 @@
-=begin 
-Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
-=end
-
 def decode_char(morseletter)
   morse_code = { '.-' => 'A', '-...' => 'B',
                  '-.-.' => 'C', '-..' => 'D',
@@ -17,22 +13,18 @@ def decode_char(morseletter)
                  '.--' => 'W', '-..-' => 'X',
                  '-.--' => 'Y', '--..' => 'Z' }
 
-  #morse_code.each do |key, value|
+  # morse_code.each do |key, value|
   #  value if key == morseletter
-  #end
+  # end
   morse_code[morseletter]
 end
 
 puts decode_char('.-')
 
-=begin 
-Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
-=end
-
 # decode_word("-- -.--") => "MY"
 def decode_word(word)
-  space = " " # 01
-  newWord = word.split(space).map {|n| decode_char(n)}.join
+  space = ' ' # 01
+  word.split(space).map { |n| decode_char(n) }.join
 end
 
-puts decode_word("-- -.--")
+puts decode_word('-- -.--')
